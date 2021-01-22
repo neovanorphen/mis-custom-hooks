f8b628c2af85b732dbc0c112f197b1b3ccf0b8a0
Felipe Navarro <felipe.navarro22@gmail.com>
2021-01-22 12:43:16 -0300

    doc

--- useCounter/useCounter.js
+++ useCounter/useCounter.js
@@ -1,6 +1,5 @@
 import { useState } from "react"
 
-
 export const useCounter = (initialState = 10) => {
     const [counter, setCounter] = useState(initialState)
 
--- useFetch/useFetch.js
+++ useFetch/useFetch.js
@@ -1,12 +1,10 @@
 import { useEffect, useState, useRef } from "react"
 
-
 export const useFetch = ( url ) => {
 
    const isMounted = useRef(true)
+
    const [state, setState] = useState({data: null, loading: true, error: null})
-
-
 
    useEffect(() => {
     return () => {
@@ -19,16 +17,13 @@ export const useFetch = ( url ) => {
         fetch(url)
             .then(resp => resp.json())
             .then(data =>{
-                
-                    if (isMounted.current){
-                        setState({
-                            loading: false,
-                            error: null,
-                            data
-                        })
-                    }  
-
-                
+                if (isMounted.current){
+                    setState({
+                        loading: false,
+                        error: null,
+                        data
+                    })
+                }  
             })
             .catch( () => {
                 setState({
